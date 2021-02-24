ea2e264416e8c0758957b3eb82d5a804c89458d0
Tiago <tiago.frioli@gmail.com>
2021-02-23 23:29:03 -0300

    migrations controller models

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,10 @@
 import express, { request, response } from 'express';
+import 'reflect-metadata';
+import './database';
+import { router } from './routes';
 
 const app = express();
-
-app.get('/', (request, response)=> {
-    return response.json({message: 'Hello world'});
-});
-
-app.post('/', (request, response)=> {
-    return response.json({message: 'Dados salvos com sucesso'});
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, ()=> console.log('Server is running'));
